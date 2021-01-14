c17aada47f089fc087398d0a0ce46453d7de8f3f
Sasha-M-Motielall <smotielall20@stuy.edu>
2021-01-13 22:03:34 -0500

    radixSortSimple and radixSort created

--- Radix.java
+++ Radix.java
@@ -8,10 +8,59 @@ public class Radix{
     if(n<0){return ans.length()-1;}
     return ans.length();
   }
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
     for(int i=0;i<buckets.length;i++){
       original.extend(buckets[i]);
     }
   }
-  
+  public static void radixSortSimple(SortableLinkedList data){
+    int largest=1;
+    SortableLinkedList dumpster = new SortableLinkedList();
+    SortableLinkedList master = data;
+    SortableLinkedList[] bucket = new SortableLinkedList[10];
+    for(int m=0;m<bucket.length;m++){
+      bucket[m]= new SortableLinkedList();
+    }
+
+    for(int q=0;q<master.size();q++){
+      if(length(master.get(q))>largest){largest=length(master.get(q));}
+    }
+
+    for(int i=0;i<largest;i++){
+      for(int l=0;l<master.size();l++){
+        bucket[nth(master.get(l), i)].add(master.get(l));
+      }
+        dumpster.extend(master);
+      for(int j=0;j<bucket.length;j++){
+        master.extend(bucket[j]);
+      }
+    }
+    data=master;
+  }
+
+  public static void radixSort(SortableLinkedList data){
+    int largest=1;
+    SortableLinkedList dumpster = new SortableLinkedList();
+    SortableLinkedList master = data;
+    SortableLinkedList[] bucket = new SortableLinkedList[19];
+    for(int m=0;m<bucket.length;m++){
+      bucket[m]= new SortableLinkedList();
+    }
+
+    for(int q=0;q<master.size();q++){
+      if(length(master.get(q))>largest){largest=length(master.get(q));}
+    }
+
+    for(int i=0;i<largest;i++){
+      for(int l=0;l<master.size();l++){
+        if(master.get(l)<0){bucket[nth(master.get(l), i)+9].add(master.get(l));}else{
+        bucket[nth(master.get(l), i)+9].add(master.get(l));}
+      }
+        dumpster.extend(master);
+      for(int j=0;j<bucket.length;j++){
+        master.extend(bucket[j]);
+      }
+    }
+    data=master;
+  }
 }
